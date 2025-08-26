69f05d0e4cdced2e75cd1f27a235ed1515d0db6b
gandumgod <rictolorio@gmail.com>
2025-08-26 15:42:14 +0800

    init admin dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import Sidebar from './components/Layout/Sidebar'
-import React from 'react'
+import Header  from './components/Layout/Header'
+import React, { useState } from 'react'
 
 function App() {
+  const [sideBarCollapsed, setSideBarCollapsed] = useState(false);
+  const [currentPage, setCurrentPage] = useState('dashboard')
   return (
     <div className='min-h-screen bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-50 dark:from-slate-900 dark:via-slate-800 dark:to-slate-900 transition-all duration-500 '>
       <div className='flex h-screen overflow-hidden'>
-        <Sidebar />
-      </div>
-    </div>
+        <Sidebar 
+        collapsed = {sideBarCollapsed} 
+        onToggle = {()=> setSideBarCollapsed(!sideBarCollapsed)}
+        currentPage = {currentPage}
+        onPageChange = {setCurrentPage} 
+        />
+        <div className='flex-1 flex-col overflow-hidden'>
+          <Header />
+        </div>
+         
+      </div> 
+    </div>  
   )
 }
 
--- src/components/Layout/Sidebar.jsx
+++ src/components/Layout/Sidebar.jsx
@@ -1,19 +1,157 @@
-import { Zap } from 'lucide-react'
 import React from 'react'
+import {
+  BarChart3,
+  Calendar,
+  ChevronDown,
+  CreditCard,
+  FileText,
+  LayoutDashboard,
+  MessageSquare,
+  Package,
+  Settings,
+  ShoppingBag,
+  Users,
+  Zap
+} from 'lucide-react'
+
+const menuItems = [
+  {
+    id: 'dashboard',
+    icon: LayoutDashboard,
+    label: 'Dashboard',
+    active: true,
+    badge: 'New',
+  },
+  {
+    id: 'analytics',
+    icon: BarChart3,
+    label: 'Analytics',
+    submenu: [
+      { id: 'overview', label: 'Overview' },
+      { id: 'reports', label: 'Reports' },
+      { id: 'insights', label: 'Insights' },
+    ]
+  },
+  {
+    id: 'users',
+    icon: Users,
+    label: 'Users',
+    submenu: [
+      { id: 'all-users', label: 'All Users' },
+      { id: 'roles', label: 'Roles and Permissions' },
+      { id: 'activity', label: 'User Activity' },
+    ]
+  },
+  {
+    id: 'ecommerce',
+    icon: ShoppingBag,
+    label: 'E-Commerce',
+    submenu: [
+      { id: 'products', label: 'Products' },
+      { id: 'orders', label: 'Orders' },
+      { id: 'customers', label: 'Customers' },
+    ]
+  },
+  {
+    id: 'inventory',
+    icon: Package,
+    label: 'Inventory',
+    count: 847,
+  },
+  {
+    id: 'transactions',
+    icon: CreditCard,
+    label: 'Transactions',
+  },
+  {
+    id: 'messages',
+    icon: MessageSquare,
+    label: 'Messages',
+    badge: '12',
+  },
+  {
+    id: 'calendar',
+    icon: Calendar,
+    label: 'Calendar',
+  },
+  {
+    id: 'reports',
+    icon: FileText,
+    label: 'Reports',
+  },
+  {
+    id: 'settings',
+    icon: Settings,
+    label: 'Settings',
+  },
+]
 
 
-function Sidebar() {
+function Sidebar({collapsed, onToggle, currentPage, onPageChange}) {
   return (
-    <div className='transition duration-300 ease-in-out bg-white/80 dark:bg-slate-900/80 backdrop-blur-xl border-r border-slate-200/50 dark:border-slate-700/50 flex flex-col relative z-10'>
+    <div className={`${
+      collapsed ? 'w-20' : 'w-72'
+      } transition duration-300 ease-in-out bg-white/80 dark:bg-slate-900/80 backdrop-blur-2xl border-r border-slate-200/50 dark:border-slate-700/50 flex flex-col relative z-10`}>
         {/* Logo */}
         <div className='p-6 border-b border-slate-200/50 dark:border-slate-700/50'>
             <div className='flex items-center space-x-3'>
-                <div className='w-10 h-10 bg-gradient-to-r from-blue-600 to-purple-600 rounded-xl flex items-center justify-centershadow-lg'>
-                    <Zap className='w-6 h-6 text-white '/>
+                <div className='w-10 h-10 bg-gradient-to-r from-blue-600 to-purple-600 rounded-xl flex items-center justify-center shadow-lg'>
+                  {/* <Zap className='text-white font-bold'/> */}
+                   <Zap className='w-6 h-6 text-white '/>
+                 </div>
+                {/* Conditional Rendering */}
+                <div>
+                  <h1 className='text-xl font-bold text-slate-800 dark:text-white'>Nexus</h1>
+                  <p className='text-xs text-slate-500 dark:text-slate-400'>
+                    Admin Panel
+                  </p>
                 </div>
             </div>
         </div>
-      
+        
+       
+       {/* Navigation 1 will display Dynamic Menus */}
+        <nav className='flex-1 p-4 space-y-2 overflow-y-auto'>    
+          {menuItems.map ((item)=>{
+          return (
+            <div key={item.id}>
+              <button className={'w-full flex items-center justify-between p-3 rounded-xl transition-all duration-200'}>
+                <div className='flex items-center space-x-3 '>
+                  <item.icon className={'w-5 h-5'} />
+                  {/* Conditional Rendering */}
+                  <>
+                    <span className='font-medium ml-2'>{item.label}
+                    
+                      {item.badge && (
+                        <span className='px-2 py-1 text-xs bg-red-500 text-white-rounded-full'>
+                          {item.badge}
+                        </span>
+                      )}
+                      {item.count && (
+                        <span className='px-2 py-1 text-xs bg-slate-200 dark:bg-slate-700 text-slate-600 dark:text-slate-300 rounded-full'>{item.count}
+                        </span>                        
+                       )}                
+
+                    </span>                    
+                  </>
+                </div>
+                {item.submenu &&  (<ChevronDown className='w-4 h-4 transition-transform '/>                
+
+               )}      
+              </button>
+
+              {/* Sub Menus */}
+              {/* <div className='ml-8 mt-2 space-y-1'>
+                {item.submenu.map((subitem)=>{
+                  return <button>{subitem.label}</button>
+                })}
+              </div> */}
+            </div>
+          )  
+          })}     
+        </nav>
+
+        {/* User Profile */}
     </div>
   )
 }
